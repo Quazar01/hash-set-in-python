f66a812920dee7f09089f6d155d80bf30db0ba90
Sami Alabed <aa225uf@student.lnu.se>
2022-11-22 11:07:18 +0100

    Count unique words using set

--- part1.py
+++ part1.py
@@ -2,4 +2,26 @@
 import os
 
 
+def unique_words(path, name):
+    # Declare an empty set
+    words = set(())
+
+    with open(path + '\\data\\' + name, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+        # Assign each word in the file to the words set.
+        for word in lines:
+            words.add(word)
+
+    return len(words)
+
+
 path = os.getcwd()
+
+# Get how many unique words for Life of Brian.
+unique_words_count = unique_words(path, 'brian_13375_words.txt')
+print(f'Life of Brian has {unique_words_count} unique words!')
+
+# Get unique words count for Swedish News
+unique_words_count = unique_words(path, 'swe_news_15103923_words.txt')
+print(f'Swedish News has {unique_words_count} unique words!')
