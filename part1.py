45844a9d04c7402bcb7d99966dd09d5256608712
Sami Alabed <aa225uf@student.lnu.se>
2022-11-22 11:17:46 +0100

    top 10 frequent words using dicts

--- part1.py
+++ part1.py
@@ -3,25 +3,68 @@ import os
 
 
 def unique_words(path, name):
-    # Declare an empty set
-    words = set(())
 
-    with open(path + '\\data\\' + name, 'r', encoding='utf-8') as file:
+    words_set = set(())
+
+    words_dict = {}
+    most_freq_words = {}
+
+    with open(path + '\\' + name, 'r', encoding='utf-8') as file:
+        # Get the lines of the file
         lines = file.readlines()
 
-        # Assign each word in the file to the words set.
+        # Assign each line/word to the set.
         for word in lines:
-            words.add(word)
+            # Strip the word from new line and add it to the set.
+            word = word.strip('\n')
+            words_set.add(word)
 
-    return len(words)
+            # Get the most 10 frequent words.
+            if len(word) > 4:
+                # If the word is not in the dict, add the word
+                # as the key, and add it's value to 1
+                if word not in words_dict.keys():
+                    words_dict[word] = 1
+
+                else:
+                    # Otherwise, increase the word's value by 1.
+                    words_dict[word] += 1
+
+        # Got the sorted function from freecodecamp.com
+        # Sort the dict by it's value in reverse
+        sorted_list = sorted(words_dict.items(),
+                             key=lambda x: x[1], reverse=True)
+
+        # Get the firt 10 key-value pairs from the list
+        for i in range(10):
+            k = sorted_list[i][0]
+            v = sorted_list[i][1]
+            # Assign the key and the value to the most_freq_words dict.
+            most_freq_words[k] = v
+
+        return len(words_set), most_freq_words
 
 
 path = os.getcwd()
 
-# Get how many unique words for Life of Brian.
-unique_words_count = unique_words(path, 'brian_13375_words.txt')
-print(f'Life of Brian has {unique_words_count} unique words!')
+# lIFE OF BRIAN
+print("Results for Life of Brian file:\n")
+unique_words_count, most_freq_words = unique_words(
+    path, 'brian_13375_words.txt')
 
-# Get unique words count for Swedish News
-unique_words_count = unique_words(path, 'swe_news_15103923_words.txt')
-print(f'Swedish News has {unique_words_count} unique words!')
+print(f'There are {unique_words_count} unique words.\n')
+print("Top 10 most frequented words:")
+for key, value in most_freq_words.items():
+    print(f"{key}: {value}")
+
+# SWEDISH NEWS
+print("\nResults for Swedish News file:\n")
+unique_words_count, most_freq_words = unique_words(
+    path, 'swe_news_15103923_words.txt')
+print(f'There are {unique_words_count} unique words.\n')
+
+print("Top 10 most frequented words:")
+for key, value in most_freq_words.items():
+    print(f"{key}: {value}")
+
+# It takes around 15 seconds to process Swedish News!!
